8d3e657fec76729428da0c653d910d3e6fdbc8fa
Diego Silva <diego.alveslv@gmail.com>
2024-05-22 16:11:25 -0300

    add loading with react/Suspense

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -6,27 +6,36 @@ import {
   getNewsForYearAndMonth,
 } from "@/lib/news";
 import Link from "next/link";
+import { Suspense } from "react";
 
-export default function FilteredNewsPage({ params }) {
-  const filter = params.filter;
-
-  const selectedYear = filter?.[0];
-  const selectedMonth = filter?.[1];
-  let links = getAvailableNewsYears();
-
+async function FilteredNews({ selectedYear, selectedMonth }) {
   let news;
   if (selectedYear && !selectedMonth) {
-    news = getNewsForYear(selectedYear);
-    links = getAvailableNewsMonths(selectedYear);
+    news = await getNewsForYear(selectedYear);
   } else if (selectedYear && selectedMonth) {
-    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
-    links = [];
+    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
   }
 
   let newsContent = <p>No news found for the selected period.</p>;
 
   if (news && news.length > 0) {
     newsContent = <NewsList news={news} />;
+  }
+
+  return newsContent;
+}
+
+export default async function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    links = getAvailableNewsMonths(selectedYear);
+  } else if (selectedYear && selectedMonth) {
+    links = [];
   }
 
   if (
@@ -56,7 +65,12 @@ export default function FilteredNewsPage({ params }) {
           </ul>
         </nav>
       </header>
-      {newsContent}
+      <Suspense fallback={<p>Loading news...</p>}>
+        <FilteredNews
+          selectedYear={selectedYear}
+          selectedMonth={selectedMonth}
+        />
+      </Suspense>
     </>
   );
 }
