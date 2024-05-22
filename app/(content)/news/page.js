47609ae425422744fac233669d5ac18d9b0a75ca
Diego Silva <diego.alveslv@gmail.com>
2024-05-22 15:44:30 -0300

    client-side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,50 @@
+"use client";
+
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [news, setNews] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Error to fetch news.");
+        setLoading(false);
+        return;
+      }
+
+      const responseNews = await response.json();
+      setNews(responseNews);
+      setLoading(false);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
